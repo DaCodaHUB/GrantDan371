4739fe8de5a90edf1e1b9af718fd1cb9dee67f91
Danny <dannylpov@gmail.com>
2017-04-21 03:58:44 -0700

    Add files via upload

--- lab1new/DE1_SoC.sv
+++ lab1new/DE1_SoC.sv
@@ -1,21 +1,19 @@
 //----------------------------------
-//Module: D Flip Flop
+//Module: DE1_SoC
 //Designed by: Grant Maiden, Dang Le
 //Purpose: DFF for 371 labs
 //----------------------------------
 
 
-module dff (
-d, //d input
-q, //q output
-clk); //clock
-
-	input d, f, clk;
+module DE1_SoC (CLOCK_50, SW, LEDR);
+	input CLOCK_50;  // 50MHz clock.
+	input [9:0] SW;
+	output [3:0] LEDR;
 	
-	always_ff @(posedge clk)
-		if (reset)
-			d <= 0;
-		else
-			q<= d;
-	end
+	// Generate clk off of CLOCK_50, whichClock picks rate.
+	wire [31:0] clk;
+	parameter whichClock = 0;
+	clock_divider cdiv (CLOCK_50, clk);
+	
+	RippleUp r1 (.Clk(clk[whichClock]), .Reset(SW[9]), .out(LEDR));
 endmodule
