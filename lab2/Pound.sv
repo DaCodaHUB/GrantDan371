f75580eec9a3243b7187504660502eb65a5581d9
GrantMaiden <granttm@gmail.com>
2017-05-04 02:44:51 -0700

    Add files via upload

--- lab2/Pound.sv
+++ lab2/Pound.sv
@@ -4,53 +4,48 @@ module Pound (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	input logic CLOCK_50;
 	input logic [3:0] KEY;
 	input logic [9:0] SW;
-	//boolean flag values
-	integer resetFlag, stateLevelHigh, stateLevelLow, stateIncreasingWater, stateDecreasingWater, stateArriving, stateDeparting;
 	
 	//INITIALIAZE Hex displays
 	initial begin
 	HEX3 = 7'b1111111;
 	end
 	
-	//METASTABILITY for user inputs
-	logic METAwaterUp, METAwaterDown, METAarriving, METAdeparting, METAoutsideLock, METAinsideLOCK, METAswitchDirection;
-	DFlipFlop waterUpFF(KEY[1], METAwaterUp, reset, CLOCK_50);
-	DFlipFlop waterDownFF(KEY[2], METAwaterDown, reset, CLOCK_50);
-	DFlipFlop arrivingFF(SW[0], METAarriving, reset, CLOCK_50);
-	DFlipFlop departingFF(SW[1], METAdeparting, reset, CLOCK_50);
-	DFlipFlop outsideLockFF(SW[2], METAoutsideLock, reset, CLOCK_50);
-	DFlipFlop insideLockFF(SW[3], METAinsideLOCK, reset, CLOCK_50);
-	DFlipFlop switchDirectionFF(SW[4], METAswitchDirection, reset, CLOCK_50);
-		
 	//reset control
 	logic reset;
 	
+	//METASTABILITY for user inputs
+	logic METAwaterUp, METAwaterDown, METAarriving, METAdeparting, METAoutsideLock, METAinsideLOCK, METAswitchDirection;
+	
+	DFlipFlop waterUpFF(KEY[1], METAwaterUp, reset, clk[whichClock]);
+	DFlipFlop waterDownFF(KEY[2], METAwaterDown, reset, clk[whichClock]);
+	DFlipFlop arrivingFF(SW[0], METAarriving, reset, clk[whichClock]);
+	DFlipFlop departingFF(SW[1], METAdeparting, reset, clk[whichClock]);
+	DFlipFlop outsideLockFF(SW[2], METAoutsideLock, reset, clk[whichClock]);
+	DFlipFlop insideLockFF(SW[3], METAinsideLock, reset, clk[whichClock]);
+	DFlipFlop switchDirectionFF(SW[4], METAswitchDirection, reset, clk[whichClock]);
+	
 	//integer values
 	integer waterLevelHeight;
-	integer boatInLock;
 	
 	//reset declaration
 	assign reset = ~KEY[0];
 
 	//Clock setup
-	parameter whichClock = 0;
+	parameter whichClock = 19;
 	logic [31:0]clk;
 	clock_divider cdiv1 (CLOCK_50, clk);
 	
 	//ledControl
-	logic [3:0]ledState;
-	assign LEDR[3:0] = ledState[3:0];
-	assign LEDR[9:4] = 6'b0;
+	assign LEDR[9:5] = 4'b0;
 	
 	//counter control
-	integer timeCounter, timerOnFlag;
+	integer timeCounter;
 	
 	//Mux HEX control for feet conversion
 	logic [6:0]muxHexSel;
-	logic [6:0]increasingHEX5, increasingHEX4;
-	logic [6:0]decreasingHEX5, decreasingHEX4;
-	mux2_1 hex4Converter(.out(HEX4), .i0(increasingHEX4), .i1(decreasingHEX4), .sel(muxHexSel));
-	mux2_1 hex5Converter(.out(HEX5), .i0(increasingHEX5), .i1(decreasingHEX5), .sel(muxHexSel));
+	logic [6:0]waterHEX5, waterHEX4;
+	assign HEX5 = waterHEX5;
+	assign HEX4 = waterHEX4;
 	
 	//HEX control for Arrival timer
 	integer minutes,tens,ones;
@@ -101,182 +96,228 @@ module Pound (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	
 	//HEX control for waterLevel while increasing
 	integer feetOnes, feetDecimal;
-
 	assign feetOnes = waterLevelHeight / 11200;
 	assign feetDecimal = ((waterLevelHeight/1120) % 10);
 	
 	always_comb
 		case (feetOnes)
 			// 	 Light: 6543210
-			0: increasingHEX5 = ~7'b0111111; // 0
-			1: increasingHEX5 = ~7'b0000110; // 1
-			2: increasingHEX5 = ~7'b1011011; // 2
-			3: increasingHEX5 = ~7'b1001111; // 3
-			4: increasingHEX5 = ~7'b1100110; // 4
-			5: increasingHEX5 = ~7'b1101101; // 5
-			default: increasingHEX5 = 7'b1111111;
+			0: waterHEX5 = ~7'b0111111; // 0
+			1: waterHEX5 = ~7'b0000110; // 1
+			2: waterHEX5 = ~7'b1011011; // 2
+			3: waterHEX5 = ~7'b1001111; // 3
+			4: waterHEX5 = ~7'b1100110; // 4
+			5: waterHEX5 = ~7'b1101101; // 5
+			default: waterHEX5 = 7'b1111111;
 		endcase
 	always_comb
 		case (feetDecimal)
 			// 	 Light: 6543210
-			0: increasingHEX4 = ~7'b0111111; // 0
-			1: increasingHEX4 = ~7'b0000110; // 1
-			2: increasingHEX4 = ~7'b1011011; // 2
-			3: increasingHEX4 = ~7'b1001111; // 3
-			4: increasingHEX4 = ~7'b1100110; // 4
-			5: increasingHEX4 = ~7'b1101101; // 5
-			6: increasingHEX4 = ~7'b1111101; // 6
-			7: increasingHEX4 = ~7'b0000111; // 7
-			8: increasingHEX4 = ~7'b1111111; // 8
-			9: increasingHEX4 = ~7'b1101111; // 9
-			default: increasingHEX4 = 7'b1111111;
+			0: waterHEX4 = ~7'b0111111; // 0
+			1: waterHEX4 = ~7'b0000110; // 1
+			2: waterHEX4 = ~7'b1011011; // 2
+			3: waterHEX4 = ~7'b1001111; // 3
+			4: waterHEX4 = ~7'b1100110; // 4
+			5: waterHEX4 = ~7'b1101101; // 5
+			6: waterHEX4 = ~7'b1111101; // 6
+			7: waterHEX4 = ~7'b0000111; // 7
+			8: waterHEX4 = ~7'b1111111; // 8
+			9: waterHEX4 = ~7'b1101111; // 9
+			default: waterHEX4 = 7'b1111111;
 		endcase
 	
-	//HEX control for waterlevel while decreasing
-	integer feetOnesDec, feetDecimalDec;
-	assign feetOnesDec = 1191 * waterLevelHeight / 100000;
-	assign feetDecimalDec = ((1191 * waterLevelHeight) % 100000)/10000;
-	
-	always_comb
-		case (feetOnesDec)
-			// 	 Light: 6543210
-			0: decreasingHEX5 = ~7'b0111111; // 0
-			1: decreasingHEX5 = ~7'b0000110; // 1
-			2: decreasingHEX5 = ~7'b1011011; // 2
-			3: decreasingHEX5 = ~7'b1001111; // 3
-			4: decreasingHEX5 = ~7'b1100110; // 4
-			5: decreasingHEX5 = ~7'b1101101; // 5
-			default: decreasingHEX5 = 7'b1111111;
-		endcase
-	always_comb
-		case (feetDecimalDec)
-			// 	 Light: 6543210
-			0: decreasingHEX4 = ~7'b0111111; // 0
-			1: decreasingHEX4 = ~7'b0000110; // 1
-			2: decreasingHEX4 = ~7'b1011011; // 2
-			3: decreasingHEX4 = ~7'b1001111; // 3
-			4: decreasingHEX4 = ~7'b1100110; // 4
-			5: decreasingHEX4 = ~7'b1101101; // 5
-			6: decreasingHEX4 = ~7'b1111101; // 6
-			7: decreasingHEX4 = ~7'b0000111; // 7
-			8: decreasingHEX4 = ~7'b1111111; // 8
-			9: decreasingHEX4 = ~7'b1101111; // 9
-			default: decreasingHEX4 = 7'b1111111;
-		endcase
-
-	
-	always_ff @(posedge CLOCK_50) begin
+	//Additonal Logic State declarations
+	integer waterControlEnable, waterLevelHigh, waterLevelLow, arrivingState, arrivingState2, arrivingDirectionState, boatDockedState;
+	//Flag Declarations
+	integer arrivingFlag, arrivingFlag2, LEDflashEnable;
+	//Additional Counters
+	integer ledFlashCounter;
+		
+	always_ff @(posedge clk[whichClock]) begin
 		//reset state
 		if (reset) begin //reset to low water state
 			waterLevelHeight <= 0;
-			stateLevelLow <= 1;
-			stateArriving <= 0;
-			stateLevelHigh <= 0; 
-			stateIncreasingWater<=0;
-			stateDecreasingWater<=0;
-			stateDeparting <= 0;
-			ledState <= 4'b1000;
 			timeCounter <= 0;
-			timerOnFlag <= 0;
-			boatInLock <= 0;
+			LEDR[0] <= 0;
+			LEDR[1] <= 0;
+			LEDR[2] <= 1;
+			LEDR[3] <= 1;
+			LEDR[4] <= 0;
+			waterControlEnable <= 1;
+			arrivingState <= 0;
+			arrivingState2 <= 0;
+			arrivingFlag <= 0;
+			arrivingFlag2 <= 0;
+			arrivingDirectionState <= 0;
+			boatDockedState <= 0;
+			LEDflashEnable <= 0;
 		end
-		if(stateLevelLow == 1) begin //ledControl
-			ledState <= 4'b1000;
+		
+		//Direction of Boat Control Begin
+		if (~METAswitchDirection && arrivingFlag == 0 && arrivingFlag2 == 0 )begin
+			LEDR[4] <= 0;
+			arrivingDirectionState <= 0;
 		end
-		if(stateLevelLow == 1 && METAarriving) begin //turn on stateIncreasingWater and stateArriving
-			ledState <= 4'b1101;
-			stateLevelLow <= 0;
-			stateIncreasingWater <= 1;
-			stateArriving <= 1;
-			timerOnFlag <= 1;
-			timeCounter <= 300;
-			muxHexSel <= 7'b0000000;
+		else if (METAswitchDirection && arrivingFlag == 0 &&  arrivingFlag2 == 0) begin
+			LEDR[4] <= 1;
+			arrivingDirectionState <= 1;
 		end
-		if(~METAinsideLOCK && ~METAoutsideLock) begin //waterlevelcontrol ON until 4.7 height or 0.3 height
-			if (waterLevelHeight > 56000)
+		//Direction of Boat Control Begin
+		
+		//waterControl STATE block
+		if(waterControlEnable == 1) begin
+			if(METAoutsideLock && 0 <= waterLevelHeight && waterLevelHeight <= 4536) begin //if inbetween 0.0 and 0.3
+				waterLevelLow <= 1;
+				waterControlEnable <= 0;
+				waterLevelHeight <= 0;
+				//LEDR[2] <= 1'b0;
+			end
+			if(METAinsideLock && 52640 <= waterLevelHeight && waterLevelHeight <= 56000) begin //if inbetween 4.7 and 5.0
+				waterLevelHigh <= 1;
+				waterControlEnable <= 0;
 				waterLevelHeight <= 56000;
-			else if (~METAwaterUp && ~METAwaterDown)
-				waterLevelHeight <= waterLevelHeight;
-			else begin
-				if (~METAwaterUp)begin
-					waterLevelHeight <= waterLevelHeight + 112;
+				//LEDR[3] <= 1'b0;
+			end
+			else if(waterLevelHeight > 56000)
+				waterLevelHeight <= 56000;
+			else if (waterLevelHeight < 0)
+				waterLevelHeight <= 0;
+			else if(~METAwaterUp && METAwaterDown) begin
+				waterLevelHeight <= waterLevelHeight + 112;	
+			end
+			else if(METAwaterUp && ~METAwaterDown) begin
+				waterLevelHeight <= waterLevelHeight - 127;	
+			end	
+		end
+		//waterControl STATE block END
+		
+		//Turn on Arriving State, Start 5min timer
+		if (~METAswitchDirection && METAarriving && arrivingFlag == 0 && arrivingFlag2 == 0) begin
+			LEDR[0] <= 1'b1;
+			timeCounter <= 300;
+			arrivingFlag <= 1;
+			ledFlashCounter <= 0;
+			LEDflashEnable <= 1;
+		end
+		else if (METAswitchDirection && METAarriving && arrivingFlag2 == 0 && arrivingFlag == 0) begin
+			LEDR[0] <= 1'b1;
+			timeCounter <= 300;
+			arrivingFlag2 <= 1;
+			ledFlashCounter <= 0;
+			LEDflashEnable<= 1;
+		end
+		//WaterLevelHighState Begin
+		if(waterLevelHigh == 1)begin
+			if (arrivingFlag == 1) begin //boat has arrived, ready to depart, going HIGH TO LOW
+				if(arrivingFlag == 1 && timeCounter == 0 && LEDflashEnable == 1) begin 
+					boatDockedState <= 1;
+					ledFlashCounter <= ledFlashCounter + 1;
+					if (((ledFlashCounter/16) %2) == 0)
+						LEDR[0] <= 1'b0;
+					else 
+						LEDR[0] <= 1'b1;
 				end
-				if (~METAwaterDown && waterLevelHeight == 0)begin
-					waterLevelHeight <= 0;
-				end	
-				if (~METAwaterDown && waterLevelHeight != 0)begin
-					waterLevelHeight <= waterLevelHeight - 127;
-				end					
-			end
-		end
-		if(stateArriving == 1) begin
-			if (stateIncreasingWater == 1)
-				ledState <= 4'b1101;
-			else
-				ledState <= 4'b0101;
-		end
-		if (timerOnFlag == 1) begin
-			if(timeCounter == 0) begin
-				timeCounter <= 0;
-				stateArriving <= 1;
-			end
-			else
-				timeCounter <= timeCounter - 1;		
-		end
-		//WATER HIGH && ARRIVING BOAT
-		if(450 < waterLevelHeight && waterLevelHeight <481 && METAinsideLOCK && stateArriving) begin
-			ledState <= 4'b0101;
-			waterLevelHeight <= 480;
-			//open or close lock
-		end
-		if(450 < waterLevelHeight && waterLevelHeight < 481 && ~METAinsideLOCK && stateArriving) begin
-			ledState <= 4'b1101;
-			boatInLock <= 1;
-			//open or close lock
-		end
-		if (boatInLock == 1) begin
-			if(METAdeparting && ~METAinsideLOCK) begin //DECREASINGWATER state and stateDeparting
-				stateDecreasingWater <= 1;
-				stateDeparting <= 1;
-				stateIncreasingWater <= 0;
-				stateArriving <= 0;
-				ledState <= 4'b1110;
-				waterLevelHeight <= 420;
-				muxHexSel <= 7'b1111111;
-			end
-		end
-		if(stateDecreasingWater == 1) begin //waterlevelcontrol ON until 4.7 height
-			if (waterLevelHeight == 421)
-				waterLevelHeight <= 420;
-			else if (~METAwaterUp && ~METAwaterDown)
-				waterLevelHeight <= waterLevelHeight;
-			else begin
-				if (~METAwaterUp)begin
-					waterLevelHeight <= waterLevelHeight + 1;
+				if (METAdeparting && arrivingFlag == 1 && boatDockedState == 1) begin
+					LEDR[0] <= 1'b0;
+					LEDR[1] <= 1'b1;
 				end
-				if (~METAwaterDown && waterLevelHeight == 0)begin
-					waterLevelHeight <= 0;
-				end	
-				if (~METAwaterDown && waterLevelHeight != 0)begin
-					waterLevelHeight <= waterLevelHeight - 1;
+				if (METAinsideLock)
+					LEDR[3] <= 1'b0;
+				else if (~METAinsideLock) begin
+					LEDR[3] <= 1'b1;
+					//LEDR[0] <= 1'b0;
+					waterLevelHigh <= 0;
+					waterControlEnable <= 1;
 				end
 			end
+			else if (arrivingFlag2 == 1) begin //BOAT LEAVING (LOW TO HIGH)
+				if(arrivingFlag2 == 1) begin
+					LEDR[1] <= 1'b0;
+					boatDockedState <= 0;
+					arrivingFlag2 <= 0;
+				end
+				if(METAinsideLock)
+					LEDR[3] <= 1'b0;
+				else if (~METAinsideLock) begin
+					LEDR[3] <= 1'b1;
+					waterLevelHigh <= 0;
+					waterControlEnable <= 1;
+				end
+			end
+			else begin //NO BOAT DEPARTING
+				if (METAinsideLock)
+					LEDR[3] <= 1'b0;
+				else if (~METAinsideLock) begin
+					LEDR[3] <= 1'b1;
+					waterLevelHigh <= 0;
+					waterControlEnable <= 1;
+				end
+			end
 		end
-		if(0 < waterLevelHeight && waterLevelHeight <26 && METAoutsideLock && stateDeparting) begin
-			ledState <= 4'b1000;
-			waterLevelHeight <= 480;
-			stateDecreasingWater <= 0;
-			stateDeparting <= 0;
-			boatInLock <= 0;
-			//open or close lock
+		//WaterLevelHighState END
+		
+		//WaterLevel LOW state Begin
+		if(waterLevelLow == 1) begin
+			if (arrivingFlag == 1) begin //Boat is departing (HIGH TO LOW)
+				if(arrivingFlag == 1) begin
+					LEDR[1] <= 1'b0;
+					boatDockedState <= 0;
+					arrivingFlag <= 0;
+				end
+				if(METAoutsideLock)
+					LEDR[2] <= 1'b0;
+				else if (~METAoutsideLock) begin
+					LEDR[2] <= 1'b1;
+					waterLevelLow <= 0;
+					waterControlEnable <= 1;
+				end
+			end
+			else if (arrivingFlag2 == 1) begin //Boat is arriving (LOW to HIGH)
+				if(arrivingFlag2 == 1 && timeCounter == 0 && LEDflashEnable == 1) begin 
+					boatDockedState <= 1;
+					ledFlashCounter <= ledFlashCounter + 1;
+					if (((ledFlashCounter/16) %2) == 0)
+						LEDR[0] <= 1'b0;
+					else 
+						LEDR[0] <= 1'b1;
+				end
+				if (METAdeparting && arrivingFlag2 == 1 && boatDockedState == 1) begin
+					LEDR[0] <= 1'b0;
+					LEDR[1] <= 1'b1;
+				end
+				if (METAoutsideLock)
+					LEDR[2] <= 1'b0;
+				else if (~METAoutsideLock) begin
+					LEDR[2] <= 1'b1;
+					//LEDR[0] <= 1'b0;
+					waterLevelLow <= 0;
+					waterControlEnable <= 1;
+				end
+			end
+			else begin //No boat arriving
+				if(METAoutsideLock)
+					LEDR[2] <= 1'b0;
+				else if (~METAoutsideLock) begin
+					LEDR[2] <= 1'b1;
+					waterLevelLow <= 0;
+					waterControlEnable <= 1;
+				end
+			end
 		end
-		if(0 < waterLevelHeight && waterLevelHeight < 26 && ~METAoutsideLock && stateDeparting) begin
-			ledState <= 4'b1100;
-			//open or close lock
+		//WaterLevel LOW State END
+		
+		//CounterControl BEGIN
+		if(timeCounter > 0 && ~reset) begin
+			timeCounter <= timeCounter - 1;
 		end
-		//if (METAarriving && waterLevelHigh == 1)
-		//if (METAarriving && waterLevelLow == 1)
+		//CounterCOntrol END
+		
+		//Stop Flashing dock lights
+		if (METAdeparting && boatDockedState == 1) begin
+					LEDR[0] <= 1'b0;
+					LEDflashEnable <= 0;
 		end
+	end
 endmodule
 
 module Pound_Testbench();
@@ -302,14 +343,14 @@ module Pound_Testbench();
 		KEY[0] <= 0; 			@(posedge clk);
 		KEY[0] <= 1; 			@(posedge clk);
 		KEY[1] <= 1;			@(posedge clk);
-		SW[1]	<= 1;							@(posedge clk);
-		KEY[2] <= 1;							@(posedge clk);
+		SW[1]	<= 1;				@(posedge clk);
+		KEY[2] <= 1;			@(posedge clk);
 									@(posedge clk);
 		SW[0] <= 1;				@(posedge clk);
 		KEY[1] <= 0;			@(posedge clk);
 									@(posedge clk);
 		SW[0]	<= 0;				@(posedge clk);
-					@(posedge clk);
+									@(posedge clk);
 		SW[3] <= 1;
 		for(i = 0; i <480; i++) begin
 			@(posedge clk);
